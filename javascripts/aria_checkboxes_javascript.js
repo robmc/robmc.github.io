f890934b59d9bfb423e0fe9db282f1c2fb1dbd42
Robert McCaffery <robert.j.mccaffery@gmail.com>
2018-12-15 18:56:39 -0500

    Add files via upload

---
+++ javascripts/aria_checkboxes_javascript.js
@@ -0,0 +1,21 @@
+// This function binds the event keycode 32 (space bar) to run the function toggleState
+// This is needed since the default functionality of a check box is triggered with the space bar
+function ARIA_Checkbox_Key(event) {
+  if(event.keyCode == 32){
+    toggleState(event.target)
+  }
+}
+
+// This function gets the aria-checked attribute of an element. If it is false, it makes it true and vice versa.
+function toggleState(el) {
+     var img = el.getElementsByTagName('img')[0],
+  getvalue = el.getAttribute("aria-checked");
+
+  if (getvalue == "false") {
+    el.setAttribute("aria-checked", "true");
+    img.setAttribute("src", "checked.png");
+  } else {
+    el.setAttribute("aria-checked", "false");
+    img.setAttribute("src", "unchecked.png");
+  }
+}
